21952db6ff4ce295673834d9c7f339f8c5915b6e
Rando <tirirando14@gmail.com>
2026-05-20 16:07:42 +0300

    Lisatud: ProfileTypeMapper.cs, mis teisendab ProfileSummary.Type stringi olemasolevaks ProfileTypeEnum väärtuseks ja pakub IsMainUser / IsPatient helperid.

---
+++ MedScan/MedScan.Shared/Models/ProfileTypeMapper.cs
@@ -0,0 +1,23 @@
+using MedScan.Shared.Models.Enums;
+
+namespace MedScan.Shared.Models;
+
+public static class ProfileTypeMapper
+{
+    public static ProfileTypeEnum? Parse(string? value)
+    {
+        return Enum.TryParse<ProfileTypeEnum>(value, ignoreCase: true, out var parsed)
+            ? parsed
+            : null;
+    }
+
+    public static bool IsMainUser(string? value)
+    {
+        return Parse(value) == ProfileTypeEnum.Ise;
+    }
+
+    public static bool IsPatient(string? value)
+    {
+        return Parse(value) == ProfileTypeEnum.Patsient;
+    }
+}
